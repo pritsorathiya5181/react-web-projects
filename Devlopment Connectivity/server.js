8b90d10eb2e22e89f7653a4ec41fe0f549763d2c
prit <prit.sorathiya@gmail.com>
2020-08-09 15:31:40 +0530

    implement socket.io and genrate boradcast messages

--- Devlopment Connectivity/server.js
+++ Devlopment Connectivity/server.js
@@ -3,13 +3,33 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const http = require('http');
+const socketio = require('socket.io');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder (public folder)
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Run when client connects
+io.on('connection', socket => {
+
+    socket.emit('message', 'Welcome to devlopment community chat room!');
+    {/* difference is .emit only show message to user and
+         .broadcast.email is show message to all user expect currentUser */ }
+
+    // Broadcast when user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Run whens client disconnect
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+})
 
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
@@ -17,4 +37,4 @@ if (process.env.NODE_ENV == 'development') {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`.blue.bold));
+server.listen(PORT, console.log(`Server running on port ${PORT}`.blue.bold));
