eb401ab8a80eb937601d214d905a9ec13a34780a
prit <prit.sorathiya@gmail.com>
2020-08-10 18:19:18 +0530

    add google oauth2 functionality

--- STORYBOOKS/app.js
+++ STORYBOOKS/app.js
@@ -4,10 +4,15 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport')
+const session = require('express-session');
 
 const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+// passport config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -20,9 +25,21 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//passport middleware for authentication
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
---
+++ STORYBOOKS/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/dashboard');
+    });
+
+module.exports = router;
