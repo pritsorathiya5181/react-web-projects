89b7e455dd03a0dc46e2302e7ee196f892dac337
prit <prit.sorathiya@gmail.com>
2020-08-13 18:55:28 +0530

    done: complete app

--- STORYBOOKS/routes/stories.js
+++ STORYBOOKS/routes/stories.js
@@ -31,6 +31,27 @@ router.get('/', authMid.ensureAuth, (req, res) => {
                 stories: stories
             })
         })
+        .catch(err => {
+            console.log(err);
+            res.render('error/500')
+        })
+})
+
+router.get('/:id', authMid.ensureAuth, (req, res) => {
+    Story.findOne({ _id: req.params.id })
+        .populate('user').lean()
+        .then(story => {
+            if (!story) {
+                return res.render('error/404')
+            }
+            res.render('stories/show', {
+                story: story
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.render('error/404')
+        })
 })
 
 router.get('/edit/:id', authMid.ensureAuth, (req, res) => {
@@ -47,6 +68,66 @@ router.get('/edit/:id', authMid.ensureAuth, (req, res) => {
                 })
             }
         })
+        .catch(err => {
+            console.log(err);
+            res.render('error/500')
+        })
 })
+
+router.put('/:id', authMid.ensureAuth, (req, res) => {
+    Story.findOne({ _id: req.params.id }).lean()
+        .then(story => {
+            if (!story) {
+                return res.render('error/404')
+            }
+            if (story.user != req.user.id) {
+                res.redirect('/stories')
+            } else {
+                Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                    new: true,
+                    runValidators: true
+                })
+                    .then(newStory => {
+                        story = newStory;
+                        res.redirect('/dashboard')
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.render('error/500')
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.render('error/500')
+        })
+})
+
+
+router.post('/delete/:id', authMid.ensureAuth, (req, res) => {
+    Story.remove({ _id: req.params.id })
+        .then(result => {
+            res.redirect('/dashboard')
+        })
+        .catch(err => {
+            console.log(err);
+            res.render('error/500')
+        })
+})
+
+router.get('/user/:userId', authMid.ensureAuth, (req, res) => {
+    Story.find({ user: req.params.userId, status: 'public' })
+        .populate('user').lean()
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.render('error/500')
+        })
+})
+
 
 module.exports = router;
