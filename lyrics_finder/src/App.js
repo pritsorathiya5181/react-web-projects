417f82eb6a76e50bddc0a6344e7e9c3e3d9b441f
prit <prit.sorathiya@gmail.com>
2020-08-28 14:17:33 +0530

    done: navbar and create global provider file

--- lyrics_finder/src/App.js
+++ lyrics_finder/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
 import './App.css';
+
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import Index from './components/layout/Index';
+import Navbar from './components/layout/Navbar';
+import { Provider } from './context';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
   );
 }
 
