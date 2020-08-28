912caced4a41e16bcf112e79e290b22af261c774
prit <prit.sorathiya@gmail.com>
2020-08-28 15:11:29 +0530

    done: fetching data from musicmetch api

--- lyrics_finder/src/context.js
+++ lyrics_finder/src/context.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 const Context = React.createContext();
 
 export class Provider extends Component {
     state = {
-        track_list: [
-            { track: { track_name: 'abc' } },
-            { track: { track_name: 'abc' } }
-        ],
+        track_list: [],
         heading: 'Top 10 Tracks'
+    }
+
+    componentDidMount() {
+        axios
+            .get(
+                `http://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=46f9cc1588c5bef578864f3dbf55f52f`
+            )
+            .then(res => {
+                // console.log(res.data) 
+                this.setState({ track_list: res.data.message.body.track_list })
+            })
+            .catch(err => console.log(err))
     }
 
     render() {
